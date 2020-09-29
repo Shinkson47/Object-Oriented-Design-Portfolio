edc6c88e9d203ab334ba453211d8f410922358f5
Shinkson47 <business@shinkson47.in>
2020-09-29 17:32:37 +0100

    Create project
    Created Intellij IDEA project, imported it into an eclipse project so that DMU have something to open.
    
    Added provided Converter.java, modified to be functional - and better.
    
    Attempted to add OPEX for JavaFX abstracts and it's utilities - IDEA works with it perfectly fine as expected but eclipse shits itself. The workarounds to make eclipse work don't translate between the project worked on in IDEA and the DMU test environment in eclipse so it was ditched. If i want abstracts or utilities from OPEX ill just have to copy the sources into this project directly, it's the only way to ensure comparability for when the project's opened in eclipse.
    
    Fucking eclipse.
    Intellij master race.

--- src/Prequesite/Converter.java
+++ src/Prequesite/Converter.java
@@ -1,7 +1,5 @@
 package Prequesite;
 
-import com.shinkson47.opex.backend.runtime.errormanagement.LoggerUtils;
-
 /**
  * Provided introductory test / example, modified to be functional.
  *
@@ -10,7 +8,7 @@ import com.shinkson47.opex.backend.runtime.errormanagement.LoggerUtils;
 public class Converter {
 
     //#region constants
-    private static final String DEG_CENTI = " degrees Centigrade ";                                                     //      For ease of printing temps in either direction.
+    private static final String DEG_CENTI = " degrees Centigrade ";                                               //      For ease of printing temps in either direction.
     private static final String DEG_FEHR = " degrees Fahrenheit ";
     //#endregion
 
@@ -20,8 +18,8 @@ public class Converter {
      * @param args Command line arguments
      */
     public static void main(String[] args) {
-        printAsFahrenheit(21);                                                                                //        Print 21C as F.
-        printAsCentigrade(convertToFahrenheit(21));                                                           //        Convert 21C to F, then print the conversion of the result back to C.
+        printAsFahrenheit(21);                                                                                    //        Print 21C as F.
+        printAsCentigrade(convertToFahrenheit(21));                                                               //        Convert 21C to F, then print the conversion of the result back to C.
     }
     //#endregion
 
@@ -31,9 +29,9 @@ public class Converter {
      * @param centigrade to convert and print.
      */
     public static void printAsFahrenheit(float centigrade) {
-        printConversion(false,                                                                                   // PARAM: false,      We're printing as F, not C.
-                        centigrade,                                                                                     //        centigrade, base value
-                        convertToFahrenheit(centigrade));                                                               //        conversion, result to print.
+        printConversion(false,                                                                                    // PARAM: false,      We're printing as F, not C.
+                        centigrade,                                                                               //        centigrade, base value
+                        convertToFahrenheit(centigrade));                                                         //        conversion, result to print.
     }
 
     /**
@@ -41,9 +39,9 @@ public class Converter {
      * @param fahrenheit to convert and print.
      */
     public static void printAsCentigrade(float fahrenheit) {
-        printConversion(true,                                                                                    // PARAM: true,       We're print as C
-                        fahrenheit,                                                                                     //        fahrenheit, base value
-                        convertToCentigrade(fahrenheit));                                                               //        conversion, result to print
+        printConversion(true,                                                                                     // PARAM: true,       We're print as C
+                        fahrenheit,                                                                               //        fahrenheit, base value
+                        convertToCentigrade(fahrenheit));                                                         //        conversion, result to print
     }
 
     /**
@@ -53,9 +51,9 @@ public class Converter {
      * @param conv Conversion result
      */
     public static void printConversion(boolean isCenti, float  base, float  conv) {
-        String baseString = (isCenti) ? DEG_FEHR : DEG_CENTI;                                                           //          Unlocalized string for base value
-        String convString = (isCenti) ? DEG_CENTI : DEG_FEHR;                                                           //                             for conversion result
-        LoggerUtils.log(base + baseString + "= " + conv + convString);                                                  // OPEX:    Use Logging utility to store and print.
+        String baseString = (isCenti) ? DEG_FEHR : DEG_CENTI;                                                     //          Unlocalized string for base value
+        String convString = (isCenti) ? DEG_CENTI : DEG_FEHR;                                                     //                             for conversion result
+        System.out.println(base + baseString + "= " + conv + convString);                                            // OPEX:    Use Logging utility to store and print.
     }
     //#endregion
 
@@ -67,7 +65,7 @@ public class Converter {
      * @return Fahrenheit equivelent of <c>centigrade</c>
      */
     public static float convertToFahrenheit(float  centigrade){
-        return centigrade * 9 / 5 + 32;                                                                                 // RETURN:  c => f  ((C × 9/5) + 32 = F)
+        return centigrade * 9 / 5 + 32;                                                                           // RETURN:  c => f  ((C × 9/5) + 32 = F)
     }
 
     /**
@@ -77,7 +75,7 @@ public class Converter {
      * @return Centigrade equivelent of <c>fahrenheit</c>
      */
     public static float convertToCentigrade(float  fahrenheit){
-        return (fahrenheit - 32) * 5 / 9;                                                                               // RETURN: f => c  ((F − 32) × 5/9 = C)
+        return (fahrenheit - 32) * 5 / 9;                                                                         // RETURN: f => c  ((F − 32) × 5/9 = C)
     }
     //#endregion
 
